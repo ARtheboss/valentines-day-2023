7ee95c9bbd849035c8550c966cde46b56a738ccc
ARtheboss <advayratan@yahoo.com>
2023-06-15 14:48:06 +0530

    six month anniversary

--- main.js
+++ main.js
@@ -17,6 +17,19 @@ Reuinted - jan 15th
 my birthday - jan 24th
 Pimmy roof 2 - jan 26th
 Botanic gardens - jan 29th
+Valentines day - feb 14th
+Hard launch - feb 17th
+That day - mar 10th
+Gym - mar 15th
+Double date - mar 18th
+Birthday - apr 4th
+Beach day - apr 15th
+4/20 - apr 20th
+Cal day - apr 22th
+formal dress up - may 5th
+Move out - may 12th
+Monday = funday - may 15th
+One day of fun - may 22nd
 */
 
 const data = [
@@ -34,7 +47,20 @@ const data = [
     {title: "Back in Berkeley", text: "After a painful month apart, we're finally reunited.", image: "IMG_4032.JPG", date: new Date(2023, 0, 15)},
     {title: "My Birthday", text: "She makes me feel really special.", image: "IMG_2770.jpg", date: new Date(2023, 0, 24)},
     {title: "Pimmy Roof 2", text: "After a Foothill dinner, we revisit where it all started.", image: "IMG_2891.jpg", date: new Date(2023, 0, 26)},
-    {title: "Botanic Gardens", text: "Ignoring a brief ZipCar-related heart attack, the cutest date we've been on. Also got our best picture together.", image: "IMG_4141.JPG", date: new Date(2023, 0, 29)},
+    {title: "Botnaic Gardens", text: "Ignoring a brief ZipCar-related heart attack, the cutest date we've been on. Also got our best picture together.", image: "IMG_4141.JPG", date: new Date(2023, 0, 29)},
+    {title: "Valentine's Day", text: "From making flowers together, to this amazing website, to calculated story posts, and finally a walk—the perfect day.", image: "IMG_4271.JPG", date: new Date(2023, 1, 14)},
+    {title: "The Hard Launch", text: "Florance makes a post with me. She insists it's a soft launch. But I know better.", image: "IMG_82AAC8A475E3-1.jpeg", date: new Date(2023, 1, 17)},
+    {title: "That Day", text: "We skip all classes on a Friday and do everything our heart desires ;)", image: "IMG_4468.JPG", date: new Date(2023, 2, 10)},
+    {title: "Gym Day!", text: "I force her to go to the gym for the first time. Health is wealth.", image: "IMG_48DC9A002523-1.jpeg", date: new Date(2023, 2, 15)},
+    {title: "Double Date", text: "We go on a double date with Chris and Cat. Pretty fun. Stuff feels pretty official at this point.", image: "IMG_4517.JPG", date: new Date(2023, 3, 18)},
+    {title: "Her Birthday", text: "I plan the best birthday suprise ever.", image: "IMG_5134.jpg", date: new Date(2023, 3, 4)},
+    {title: "Beach Time", text: "With some of my friends, we all go to the beach together. It's a lot of fun. Very romantic. We do a little roleplay.", image: "IMG_5471.jpg", date: new Date(2023, 3, 15)},
+    {title: "4/20", text: "Another day we spend together, and get the best view from Evans. I love spending time with her.", image: "IMG_5802.jpg", date: new Date(2023, 3, 20)},
+    {title: "Cal Day", text: "Laying on the grass with her for an hour while that music was playing was one of the most ethereal experiences I've ever had.", image: "IMG_5828.jpg", date: new Date(2023, 3, 22)},
+    {title: "Formal Dress", text: "When we realized we both had to dress up for different reasons, we thought: 'Why not dress up together?'", image: "IMG_6174.jpg", date: new Date(2023, 4, 5)},
+    {title: "Move Out", text: "Sad moment. Our time with the dorms, where we our relationship begun, comes to an end.", image: "P1400032.jpg", date: new Date(2023, 4, 12)},
+    {title: "Monday = Funday", text: "I drive all the way down to Los Gatos to see her. We spend a great couple of hours together.", image: "P1400201.jpg", date: new Date(2023, 4, 15)},
+    {title: "Goodbye Before Summer", text: "We spend the most perfect day together, one last time before we separate for three months. And lower our rice purity score quite a bit.", image: "IMG_7022.jpg", date: new Date(2023, 4, 22)},
 ]
 
 var itemdata = [];
